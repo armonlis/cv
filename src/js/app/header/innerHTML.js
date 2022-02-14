d62fea13f4a1e4b027c494471b43c417235151d9
Siarhei Khonski <armonius@mail.ru>
2022-02-14 12:39:14 +0300

    feat: add header__main, header__contacts

--- src/js/app/header/innerHTML.js
+++ src/js/app/header/innerHTML.js
@@ -3,5 +3,19 @@ export const en = `
     <img id="app-header__photo-photo" src="./img/photo.png" alt="photo">
     <img id="app-header__photo-clip" src="./img/clip.png" alt="clip">
   </div>
+  <div id="app-header__main">
+    <h1>Siarhei Khonski</h1>
+    <h3 class="stamp">JUNIOR FRONT-END DEVELOPER</h3>
+  </div>
+  <div id="app-header__contacts">
+    <h3 class="stamp">CONTACTCS:</h3>
+    <ul>
+      <li>phone: +375(29) 661-07-92, +375(29) 761-07-92</li>
+      <li>e-mail: armonius@mail.ru</li>
+      <li>discord: Siarhei Khonski (armonlis)#3533</li>
+      <li>telegram: Siarhei Khonski</li>
+      <li>github: armonlis</li>
+    </ul>
+  </div>
 `;
 export const ru = en;
